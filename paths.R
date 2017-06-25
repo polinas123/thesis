d8a3d900dec98a4e2756435fe1efff9af51f12fa
polinas123 <polinas86@gmail.com>
2017-06-25 18:45:05 +0300

    brute force conflict fix

--- paths.R
+++ paths.R
@@ -1,10 +1,7 @@
 WEBSITES_PATH = "/Volumes/Untitled/R/Thesis/websites.csv"
 STATES_PATH = "/Volumes/Untitled/R/Thesis/states.csv"
 COLORS_PATH = "/Volumes/Untitled/R/Thesis/blue-red.csv"
-<<<<<<< HEAD
 OLD_DATA_PATH = "/Volumes/Untitled/R/Thesis/DATA"
 NEW_DATA_PATH = "/Volumes/Untitled/R/Thesis/NEW_DATA"
-=======
 DATA_PATH = "/Volumes/Untitled/R/Thesis/DATA"
->>>>>>> 8f92d9f894673e6d046d0e15b4d9935dc99fe520
 NAMES_PATH = "/Volumes/Untitled/R/Thesis/new_names.csv"
