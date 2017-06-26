a9ddb171ef2e90aa5929cbeb19a075c391df5170
polinas123 <polinas86@gmail.com>
2017-06-26 22:17:31 +0300

    adding metrics

--- tidyData.R
+++ tidyData.R
@@ -1,16 +1,17 @@
 getAggregated = function(DT, selectcol = NULL, selectval = NULL, aggrfun = ".N", grpby = NULL){
         # how many articles per day
         
-        if (!is.null(grpby)){
-                DT[!is.na(grpby), .N, by = c("publish_date", grpby)]
-        }
         if (!is.null(selectcol)){
                 setkey(DT, selectcol)
                 DT[selectval, .N, by = c("publish_date", grpby)]
         } else {
-                DT[, .N, by = c("publish_date", grpby)]
+                if (!is.null(grpby)){
+                        setkeyv(DT, grpby)
+                        DT[!NA_character_, .N, by = c("publish_date", grpby)]
+                } else {
+                        DT[, .N, by = c("publish_date")]
+                }
         }
-        
 }
 
 setAggregated = function(name, DT, selectcol = NULL, selectval = NULL, aggrfun = ".N", grpby = NULL){
